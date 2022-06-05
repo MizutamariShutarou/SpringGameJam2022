651228d22e534fff65022ba48862c84c04e66c2b
MatubaraTooru <kurakura.ingen@gmail.com>
2022-06-05 13:10:36 +0900

    Enemy作成

---
+++ Assets/Matubara/EnemyController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]float _speed;
+    [SerializeField] Transform _tgpos;
+    Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 move = _tgpos.position - transform.position;
+        _rb.velocity = move.normalized * _speed;
+    }
+}
