f5dc002fd11c72134ae1493d76b4204b347323b0
DESKTOP-A427LQ4\vantan <shutaro0415@outlook.jp>
2022-06-05 16:49:55 +0900

    GameOver追加　TimeText追加

---
+++ Assets/Tamari/Script/TimeController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeController : MonoBehaviour
+{
+    [Header("êßå¿éûä‘"), SerializeField] float _countTime;
+    [SerializeField] Text _timeText;
+    [SerializeField] GameObject _gameOverPanel;
+    [SerializeField] List<GameObject> _gameObjectList;
+
+    //bool _isGameOver;
+    void Update()
+    {
+        _countTime -= Time.deltaTime;
+
+        _timeText.text = "écÇË" + _countTime.ToString("f1");
+
+        if(_countTime <= 0)
+        {
+            _gameObjectList.ForEach(e => e.SetActive(false));
+            _timeText.text = "Ç®ë|èúäÆóπÅI";
+            _gameOverPanel.SetActive(true);
+        }
+    }
+}
